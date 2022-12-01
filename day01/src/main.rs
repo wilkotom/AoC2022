1ced4d5398c99c5c1a8734e52c8790f294cb2a69
Tom Wilkinson <tom.github@dentrasssi.net>
2022-12-01 07:27:33 +0000

    Day 01 Solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,13 +1,13 @@
 use std::{collections::BinaryHeap, io::Error};
 
-    fn main() -> Result<(), Error> {
-        let data = std::fs::read_to_string("./day01/input.txt")?;
-        let mut elves = data.split("\n\n")
-            .map(|e| e.split('\n')
-                    .map(|x| x.parse::<i32>()
-                    .unwrap_or(0)).sum())
-            .collect::<BinaryHeap<_>>();
-        println!("Part 1: {:?}", elves.peek().unwrap_or(&0));
-        println!("Part 2: {:?}", elves.pop().unwrap_or(0) + elves.pop().unwrap_or(0) + elves.pop().unwrap_or(0));
-        Ok(())
-    }
+fn main() -> Result<(), Error> {
+    let data = std::fs::read_to_string("./day01/input.txt")?;
+    let mut elves = data.split("\n\n")
+        .map(|e| e.split('\n')
+                .map(|x| x.parse::<i32>()
+                .unwrap_or(0)).sum())
+        .collect::<BinaryHeap<_>>();
+    println!("Part 1: {:?}", elves.peek().unwrap_or(&0));
+    println!("Part 2: {:?}", elves.pop().unwrap_or(0) + elves.pop().unwrap_or(0) + elves.pop().unwrap_or(0));
+    Ok(())
+}
