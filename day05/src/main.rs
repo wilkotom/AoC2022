0b85e004767689694046057035861067f9bdc8a3
Tom Wilkinson <tom.github@dentrasssi.net>
2022-12-05 12:44:55 +0000

    Minor refactor

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -13,46 +13,45 @@ struct CraneMove{
 fn main() -> Result<(), Error> {
     let in_file = std::fs::read_to_string("./day05/input.txt")?;
     let mut data = in_file.split("\n\n");
-    let cranes = parse_cranes(data.next().unwrap());
+    let stacks = parse_stacks(data.next().unwrap());
     let instructions = parse_instructions(data.next().unwrap());
-    println!("Part 1: {}", solution(cranes.clone(), instructions.clone(), false));
-    println!("Part 2: {}", solution(cranes, instructions, true));
+    println!("Part 1: {}", solution(stacks.clone(), &instructions, true));
+    println!("Part 2: {}", solution(stacks, &instructions, false));
     Ok(())
 }
 
-fn parse_cranes(starting_state: &str) -> [Vec<char>; 10] {
-    let mut cranes: [Vec<char>; 10] = Default::default();
+fn parse_stacks(starting_state: &str) -> [Vec<char>; 9] {
+    let mut stacks: [Vec<char>; 9] = Default::default();
     for line in starting_state.split('\n') {
         let mut pos = 1;
         while let Some(c) = line.chars().nth(pos) {
-            let crane = (pos -1) / 4 ;
-            pos +=4;
             if c.is_ascii_uppercase() {
-                cranes[crane].insert(0, c)
+                stacks[(pos -1) / 4].insert(0, c);
             }
+            pos +=4;
         }
     }
-    cranes
+    stacks
 }
 
 fn parse_instructions(instructions: &str) -> Vec<CraneMove> {
     instructions.split('\n').map(|x| x.parse::<CraneMove>().unwrap()).collect()
 }
 
-fn solution(mut cranes:[Vec<char>; 10], instructions: Vec<CraneMove>, part1: bool) -> String {
+fn solution(mut stacks:[Vec<char>; 9], instructions: &Vec<CraneMove>, part1: bool) -> String {
     for instruction in instructions {
         if part1 {
             for _ in 0..instruction.quantity {
-                let cargo_crate = cranes[instruction.origin -1].pop().unwrap();
-                cranes[instruction.destination-1].push(cargo_crate)
+                let cargo_crate = stacks[instruction.origin -1].pop().unwrap();
+                stacks[instruction.destination-1].push(cargo_crate)
             }
         } else {
-            let mut moved_crates = cranes[instruction.origin -1].split_off(cranes[instruction.origin -1].len() - instruction.quantity);
-            cranes[instruction.destination-1].append(&mut moved_crates);
+            let mut moved_crates = stacks[instruction.origin -1]
+                .split_off(stacks[instruction.origin -1].len() - instruction.quantity);
+            stacks[instruction.destination-1].append(&mut moved_crates);
         }
     }
-    
-    cranes.iter().filter_map(|c| c.last()).collect::<String>()
+    stacks.iter().filter_map(|c| c.last()).collect::<String>()
 }
 
 
@@ -71,19 +70,20 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2";
 
+
     #[test]
     fn test_p1() {
         let mut data = DATA.split("\n\n");
-        let cranes = parse_cranes(data.next().unwrap());
+        let stacks = parse_stacks(data.next().unwrap());
         let instructions = parse_instructions(data.next().unwrap());
-        assert_eq!(solution(cranes,instructions, true), "CMZ");
+        assert_eq!(solution(stacks,&instructions, true), "CMZ");
     }
 
     #[test]
     fn test_p2() {
         let mut data = DATA.split("\n\n");
-        let cranes = parse_cranes(data.next().unwrap());
+        let stacks = parse_stacks(data.next().unwrap());
         let instructions = parse_instructions(data.next().unwrap());
-        assert_eq!(solution(cranes,instructions, false), "MCD");
+        assert_eq!(solution(stacks,&instructions, false), "MCD");
     }
 }
